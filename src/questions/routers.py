8aa1d7adbf81f793dc159e7f6daa4c4cffe697f7
Dmitry <d.gorelov22@gmail.com>
2023-05-17 18:00:05 +0300

    корректировка вывода вопроса

--- src/questions/routers.py
+++ src/questions/routers.py
@@ -8,7 +8,7 @@ from src.utils import get_db
 
 from .crud import add_question, get_question_from_database
 from .models import Question
-from .schemas import QuestionOut
+from .schemas import LastQuestion
 from .utils import get_response
 
 
@@ -17,11 +17,11 @@ ques_router = APIRouter(prefix='/question', tags=['questions'])
 
 @ques_router.post('/',
                   summary='Запрос вопросов',
-                  response_model=QuestionOut)
+                  response_model=LastQuestion)
 def request_question(
     questions_num: int,
     db: Annotated[Session, Depends(get_db)]
-) -> QuestionOut:
+) -> LastQuestion:
     """
     Получение вопросов с API https://jservice.io/api/random?count=1,
     где questions_num: количество запрашиваемых вопросов
@@ -36,11 +36,5 @@ def request_question(
     add_question(response, db)
 
     if last_question is None:
-        return QuestionOut()
-    return QuestionOut(
-        id=last_question.id,
-        question_id=last_question.question_id,
-        question=last_question.question,
-        answer=last_question.answer,
-        created_at=last_question.created_at
-    )
+        return LastQuestion()
+    return LastQuestion(question=last_question.question)
--- src/questions/schemas.py
+++ src/questions/schemas.py
@@ -1,15 +1,10 @@
-from datetime import datetime
 from typing import Optional
 
 from pydantic import BaseModel
 
 
-class QuestionOut(BaseModel):
-    id: Optional[int]
-    question_id: Optional[int]
+class LastQuestion(BaseModel):
     question: Optional[str]
-    answer: Optional[str]
-    created_at: Optional[datetime]
 
     class Config:
         orm_mode = True
