69646d95124097db1161adc480eea8c3d09bdfd8
Dmitry <d.gorelov22@gmail.com>
2023-05-22 01:02:38 +0300

    Обновил получение сессии

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,16 @@
+from typing import Generator
 from fastapi import HTTPException
 from sqlalchemy.exc import OperationalError
 
 from src.database import SessionLocal
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    except OperationalError as e:
-        raise HTTPException(
-            status_code=503,
-            detail=f'Server closed the connection unexpectedly: {e}'
-        )
-    finally:
-        db.close()
+def get_db() -> Generator:
+    with SessionLocal() as session:
+        try:
+            yield session
+        except OperationalError as e:
+            raise HTTPException(
+                status_code=503,
+                detail=f'Server closed the connection unexpectedly: {e}'
+            )
