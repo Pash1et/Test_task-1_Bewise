8ba0f3042edcdb4394b7875d6ae6ea8d6db86808
Dmitry <d.gorelov22@gmail.com>
2023-05-22 11:57:13 +0300

    исправил тесты

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
 import pytest
+from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+from src.database import Base
 from src.main import app
 from src.utils import get_db
-from fastapi.testclient import TestClient
-from src.database import Base
 
 SQLALCHEMY_DATABASE_URL = ('postgresql://postgres_test:postgres_test@'
                            '127.0.0.1:5434/postgres_test')
@@ -16,23 +17,19 @@ TestingSessionLocal = sessionmaker(autocommit=False,
 
 
 def override_get_db():
-    Base.metadata.create_all(bind=engine)
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        db.close()
+    with TestingSessionLocal() as session:
+        yield session
 
 
 app.dependency_overrides[get_db] = override_get_db
 
 
-@pytest.fixture(autouse=True, scope='session')
+@pytest.fixture(scope='function')
 def create_db():
     Base.metadata.create_all(bind=engine)
 
 
-@pytest.fixture(autouse=True, scope='session')
+@pytest.fixture(scope='function', autouse=True)
 def drop_db():
     Base.metadata.drop_all(bind=engine)
 
--- tests/questions/test_questions.py
+++ tests/questions/test_questions.py
@@ -1,7 +1,7 @@
 from tests.questions import constants_question as const
 
 
-def test_first_post(client):
+def test_first_post(client, create_db):
     """Добавление первого вопроса в БД."""
     response = client.post(f'/question/?questions_num={const.COUNT}')
 
@@ -9,8 +9,9 @@ def test_first_post(client):
     assert response.json()['question'] is None
 
 
-def test_second_post(client):
+def test_second_post(client, create_db):
     """Добавление второго и последующих вопросов в БД."""
+    response = client.post(f'/question/?questions_num={const.COUNT}')
     response = client.post(f'/question/?questions_num={const.COUNT}')
 
     assert response.status_code == 200
